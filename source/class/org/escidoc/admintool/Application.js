6771bfc31b63b321b07634e21cd2cbbda39e2aaf
crh <christian.herlambang@fiz-karlsruhe.de>
2010-01-25 13:24:37 +0000

    these functions belong to Resource class=>deleted.

--- source/class/org/escidoc/admintool/Application.js
+++ source/class/org/escidoc/admintool/Application.js
@@ -159,63 +159,6 @@ qx.Class.define("org.escidoc.admintool.Application", {
             
             return navigationTree;
         },
-        createTable: function(){
-            // Create the initial data
-            var rowData = this.createRandomRows(10);
-            
-            // table model
-            var tableModel = new qx.ui.table.model.Simple();
-            
-            tableModel.setColumns(["Resource ID", "Name", "Created On", "Active", "a Column", "a Column", "a Column", "a Column", "a Column", "a Column"]);
-            tableModel.setData(rowData);
-            tableModel.setColumnEditable(1, true);
-            tableModel.setColumnEditable(2, true);
-            tableModel.setColumnSortable(3, false);
-            
-            // table
-            var table = new qx.ui.table.Table(tableModel);
-            
-            table.set({
-                width: 600,
-                height: 400,
-                decorator: null
-            });
-            
-            table.getSelectionModel().setSelectionMode(qx.ui.table.selection.Model.MULTIPLE_INTERVAL_SELECTION);
-            
-            var tcm = table.getTableColumnModel();
-            
-            // Display a checkbox in column 3
-            tcm.setDataCellRenderer(3, new qx.ui.table.cellrenderer.Boolean());
-            
-            // use a different header renderer
-            tcm.setHeaderCellRenderer(2, new qx.ui.table.headerrenderer.Icon("icon/16/apps/office-calendar.png", "A date"));
-            
-            return table;
-        },
-        createRandomRows: function(numberOfRows){
-            var now = new Date().getTime();
-            var dateRange = 365 * 24 * 60 * 60 * 1000;
-            var userId = 0;
-            
-            var rowData = [];
-            for (var rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
-                //What this line of code actually doing? => refactor to a function
-                var date = new Date(now + Math.random() * dateRange - dateRange / 2);
-                rowData.push([userId++, Math.random() * 10000, date, (Math.random() > 0.5)]);
-            }
-            return rowData;
-        },
-        addToolbar: function(){
-            var toolbar = new qx.ui.toolbar.ToolBar();
-            var createNewResourceButton = new qx.ui.toolbar.Button("Create New Resource");
-            
-            createNewResourceButton.addListener("execute", function(){
-                this.fireEvent("addNewResource");
-            }, this);
-            toolbar.add(createNewResourceButton);
-            return toolbar;
-        },
         // Displays the appropriate tabs for a navigationTree entry.
         showFolderView: function(folder, tab){
             var label = folder.getLabel();
