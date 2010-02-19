ddfbc2289a78c4fe1d6ca03b051d904d67a3748c
crh <christian.herlambang@fiz-karlsruhe.de>
2010-02-19 12:50:30 +0000

    [Add] properties for this object.

--- source/class/org/escidoc/admintool/wrapper/UserAccount.js
+++ source/class/org/escidoc/admintool/wrapper/UserAccount.js
@@ -5,7 +5,25 @@ qx.Class.define("org.escidoc.admintool.wrapper.UserAccount", {
 			extend : qx.core.Object,
 			construct : function(data) {
 				this._data = data;
+                this.name = data.name;
 			},
+			properties : {
+				name : {
+					check : "String",
+					event : "changeName",
+					nullable : false
+				},
+
+				loginName : {
+					check : "String",
+					event : "changeLoginName"
+				},
+				email : {
+					check : "Boolean",
+					event : "changeEmail"
+				}
+			}
+            ,
 			members : {
 				getName : function() {
 					return this._data.name;
