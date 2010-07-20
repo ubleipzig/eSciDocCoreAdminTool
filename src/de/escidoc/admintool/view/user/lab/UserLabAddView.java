f80ff1d023b2fd713ff9f3e1ddb3780119153ed0
asp <asp>
2010-07-20 15:50:39 +0000

    New layout for the add a new user panel. Improved exception handling.

--- src/de/escidoc/admintool/view/user/lab/UserLabAddView.java
+++ src/de/escidoc/admintool/view/user/lab/UserLabAddView.java
@@ -1,132 +1,117 @@
 package de.escidoc.admintool.view.user.lab;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.vaadin.data.util.ObjectProperty;
 import com.vaadin.terminal.UserError;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.Form;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.TextField;
 
 import de.escidoc.admintool.app.AdminToolApplication;
 import de.escidoc.admintool.service.UserService;
 import de.escidoc.admintool.view.ViewConstants;
+import de.escidoc.admintool.view.util.LayoutHelper;
+import de.escidoc.admintool.view.validator.EmptyFieldValidator;
 import de.escidoc.core.client.exceptions.EscidocException;
 import de.escidoc.core.client.exceptions.InternalClientException;
 import de.escidoc.core.client.exceptions.TransportException;
 import de.escidoc.core.resources.aa.useraccount.UserAccount;
 
-public class UserLabAddView extends Form implements ClickListener {
-
-    private final AdminToolApplication app;
-
+public class UserLabAddView extends CustomComponent implements ClickListener {
+	private static final long serialVersionUID = 3007285643463919742L;
+	 private static final Logger log =
+	        LoggerFactory.getLogger(UserLabAddView.class);
     private final UserLabListView userLabList;
-
     private final UserService userService;
-
+    private HorizontalLayout footer;
+    private final Button save = new Button("Save", (ClickListener) this);
+    private final Button cancel = new Button("Cancel", (ClickListener) this);
+    private TextField nameField; 
+    private TextField loginNameField;
+    private ObjectProperty nameProperty;
+    private ObjectProperty loginNameProperty;
+    
     public UserLabAddView(final AdminToolApplication app,
         final UserLabListView userLabList, final UserService userService) {
-        addStyleName("view");
-        this.app = app;
         this.userLabList = userLabList;
         this.userService = userService;
-        buildUI();
+        init();
     }
 
-    private void buildUI() {
-        setCaption("Add a new User Account");
-        addNameField().addLoginNameField();
-        // .addGrantAddView();
-        addFooter();
-        setWriteThrough(false);
-        setInvalidCommitted(false);
-        // setValidationVisible(true);
+    public void init(){
+    	Panel panel = new Panel();
+    	FormLayout form = new FormLayout();
+    	panel.setContent(form);
+    	form.setSpacing(false);
+    	panel.setCaption("Add a new User Account");
+    	panel.addComponent(LayoutHelper.create(ViewConstants.NAME_LABEL, nameField = new TextField(), "100px", true));
+        nameProperty = new ObjectProperty("", String.class);
+        nameField.setPropertyDataSource(nameProperty);
+       
+        panel.addComponent(LayoutHelper.create(ViewConstants.LOGIN_NAME_LABEL, loginNameField = new TextField(), "100px", true));
+        
+        loginNameProperty = new ObjectProperty("", String.class);
+        loginNameField.setPropertyDataSource(loginNameProperty);
+        panel.addComponent(addFooter());
+    	setCompositionRoot(panel);
     }
 
-    private UserLabAddView addNameField() {
-        final TextField nameField = new TextField(ViewConstants.NAME_LABEL);
-        addField(ViewConstants.NAME_ID, nameField);
-        nameField.setRequired(true);
-        nameField.setRequiredError("Please enter a " + ViewConstants.NAME_ID);
-        return this;
-    }
-
-    private UserLabAddView addLoginNameField() {
-        final TextField loginNameField =
-            new TextField(ViewConstants.LOGIN_NAME_LABEL);
-        addField(ViewConstants.LOGIN_NAME_ID, loginNameField);
-        loginNameField.setRequired(true);
-        loginNameField.setRequiredError("Please enter a "
-            + ViewConstants.LOGIN_NAME_ID);
-        return this;
-    }
-
-    private UserLabAddView addGrantAddView() {
-        getLayout().addComponent(new GrantAddView());
-        return this;
-    }
-
-    private HorizontalLayout footer;
-
-    private final Button save = new Button("Save", (ClickListener) this);
-
-    private final Button cancel = new Button("Cancel", (ClickListener) this);
-
-    private void addFooter() {
+    private HorizontalLayout addFooter() {
         footer = new HorizontalLayout();
         footer.setSpacing(true);
-        footer.addComponent(save);
-        footer.addComponent(cancel);
+        footer.addComponent(LayoutHelper.create(save));
+        footer.addComponent(LayoutHelper.create(cancel));
         footer.setVisible(true);
-        setFooter(footer);
+        return footer;
     }
-
+    
     public void buttonClick(final ClickEvent event) {
         final Button source = event.getButton();
 
         if (source == cancel) {
-            discard();
-            // TODO fix these, this.discard should actually do the trick.
-            getField(ViewConstants.NAME_ID).setValue("");
-            getField(ViewConstants.LOGIN_NAME_ID).setValue("");
-            setVisible(false);
-            setReadOnly(true);
+        	nameField.setValue("");
+        	loginNameField.setValue("");
         }
         else if (source == save) {
-            setValidationVisible(true);
-            validate();
-            if (isValid()) {
+        	boolean valid = true;
+        	valid = EmptyFieldValidator.isValid(nameField, "Please enter a " + ViewConstants.NAME_ID);
+        	valid &= (EmptyFieldValidator.isValid(loginNameField, "Please enter a " + ViewConstants.LOGIN_NAME_ID));
+        	
+        	if(valid){
+        		nameField.setComponentError(null);
+        		loginNameField.setComponentError(null);
+        		
                 try {
                     final UserAccount createdUserAccount =
-                        userService.create((String) getField(
-                            ViewConstants.NAME_ID).getValue(),
-                            (String) getField(ViewConstants.LOGIN_NAME_ID)
-                                .getValue());
-                    commit();
+                        userService.create((String)nameProperty.getValue(), (String)loginNameProperty.getValue());
                     userLabList.addUser(createdUserAccount);
                     userLabList.select(createdUserAccount);
-
-                    getField(ViewConstants.NAME_ID).setValue("");
-                    getField(ViewConstants.LOGIN_NAME_ID).setValue("");
-                }
-                // TODO log exception
-                // TODO report appropriate error message to the user.
-                catch (final EscidocException e) {
-                    ((TextField) getField(ViewConstants.LOGIN_NAME_ID))
-                        .setComponentError(new UserError(
-                            "A user with login name "
-                                + getField(ViewConstants.LOGIN_NAME_ID)
-                                    .getValue() + " already exist."));
+                	nameField.setValue("");
+                	loginNameField.setValue("");
+                } catch (final EscidocException e) {
+                	String error = "A user with login name "
+                        + (String)nameProperty.getValue()+ " already exist.";
+                	
+                    loginNameField.setComponentError(new UserError(error));
                     e.printStackTrace();
+                	log.error(error, e);
                 }
                 catch (final InternalClientException e) {
+                	log.error("An unexpected error occured! See log for details.", e);
                     e.printStackTrace();
                 }
                 catch (final TransportException e) {
+                	log.error("An unexpected error occured! See log for details.", e);
                     e.printStackTrace();
                 }
             }
         }
-
     }
 }
