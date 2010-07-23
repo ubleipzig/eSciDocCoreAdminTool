25670d0adac82731aebdfd8e5bb6d23145c81db5
asp <asp>
2010-07-23 12:50:50 +0000

    Extended usage for accordion.

--- src/de/escidoc/admintool/view/util/LayoutHelper.java
+++ src/de/escidoc/admintool/view/util/LayoutHelper.java
@@ -3,11 +3,14 @@
  */
 package de.escidoc.admintool.view.util;
 
+import com.vaadin.ui.Accordion;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.themes.Reindeer;
 
 /**
  * @author ASP
@@ -43,12 +46,63 @@ public class LayoutHelper {
 		return hor;
 	}
 
-	/** Helper method. Puts a blank in front of a component. Two labels with different size in front of two components.
-	 * @param label The label in front of the control.
-	 * @param comp The component to display.
-	 * @param width the fixed size of the label. The parameter has to be in CSS style, i.e. 400px for instance.
-	 * @param required should it be marked with an asterisk.
+	/** Helper method. Puts a blank in front of a component.
+	 * @param label The label in front of the controll.
+	 * @param accordion The accordion to display.
+	 * @param width the fixed size of the label. The parameter has to be in css style, i.e. 400px for instance.
+	 * @param required should it be marked with an asterix.
 	 * @return The component in an horizontal layout. A blank in front and afterwards is inserted.
+	 */
+	public static synchronized HorizontalLayout create(String label, Accordion accordion, String width, int height, boolean required){
+		HorizontalLayout hor = new HorizontalLayout();
+		hor.setHeight(height+"px");
+		hor.addComponent(new Label(" "));
+		String text = "<p align=\"right\">"+label+"</p>";
+		Label l;
+		hor.addComponent(l = new Label(text, Label.CONTENT_XHTML));
+		l.setSizeUndefined();
+		l.setWidth(width);
+		hor.setComponentAlignment(l, Alignment.MIDDLE_RIGHT);
+		if (required){
+			hor.addComponent(new Label("&nbsp;<span style=\"color:red; position:relative; top:"+(height/2-13)+"px;\">*</span>", Label.CONTENT_XHTML));
+		} else{
+			hor.addComponent(new Label("&nbsp;&nbsp;", Label.CONTENT_XHTML));
+		}
+		
+		Panel pan = new Panel();
+		pan.setSizeFull();
+		// Have it take all space available in the layout.
+		accordion.setSizeFull();
+		// Some components to put in the Accordion.
+		
+		String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<root>\n\t<X>\n\t\t<today>\n\t\t</today>\n\t\t<today/>\n\t\t<today/>\n\t</X>\n</root>"; 
+		
+		
+		for (int i=0;i<30; i++){
+			accordion.addTab(new Label(xml, Label.CONTENT_PREFORMATTED), "Tab"+i, null);
+		}
+		
+		pan.addComponent(accordion);
+		pan.setSizeUndefined();
+		pan.setWidth("800px");
+		pan.setHeight("500px");
+		pan.setStyleName( Reindeer.PANEL_LIGHT);
+		hor.addComponent(pan);
+		hor.setComponentAlignment(pan, Alignment.MIDDLE_RIGHT);
+		hor.addComponent(new Label(" "));
+		hor.setSpacing(false);
+		return hor;
+	}
+
+	/**Helper method. Puts a blank in front of a component. Two labels with different size in front of two components.
+	 * @param labelLeft the left (leading) label.
+	 * @param labelRight the right (leading) label.
+	 * @param compLeft the left component.
+	 * @param compRight the right component.
+	 * @param widthLeft the width of the left label.
+	 * @param widthRight the width of the right label.
+	 * @param required true if the component is required, otherwise false.
+	 * @return the customized component placed in a horizontal layout.
 	 */
 	public static synchronized HorizontalLayout create(String labelLeft, String labelRight, Component compLeft, Component compRight, String widthLeft, String widthRight, boolean required){
 		HorizontalLayout hor = new HorizontalLayout();
@@ -85,12 +139,14 @@ public class LayoutHelper {
 	}
 
 
-	/** Helper method. Puts two element with a label on the left in a symmetric way.
-	 * @param label The label in front of the control.
-	 * @param comp The component to display.
-	 * @param width the fixed size of the label. The parameter has to be in CSS style, i.e. 400px for instance.
-	 * @param required should it be marked with an asterisk.
-	 * @return The component in an horizontal layout. A blank in front and afterwards is inserted.
+	/** Helper method for placing components.
+	 * @param labelLeft the left (leading) label.
+	 * @param labelRight the right (leading) label.
+	 * @param compLeft the left component.
+	 * @param compRight the right component.
+	 * @param width the width of the label.
+	 * @param required true if the component is required, otherwise false.
+	 * @return the customized component placed in a horizontal layout.
 	 */
 	public static synchronized HorizontalLayout create(String labelLeft, String labelRight, Component compLeft, Component compRight, String width, boolean required){
 		HorizontalLayout hor = new HorizontalLayout();
