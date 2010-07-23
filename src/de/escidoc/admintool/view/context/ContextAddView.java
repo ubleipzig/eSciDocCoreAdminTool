d2843ac0c537e6f74c1e251cba5211815f37d23d
asp <asp>
2010-07-23 12:55:46 +0000

    New layout.

--- src/de/escidoc/admintool/view/context/ContextAddView.java
+++ src/de/escidoc/admintool/view/context/ContextAddView.java
@@ -1,46 +1,56 @@
 package de.escidoc.admintool.view.context;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.apache.commons.lang.exception.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import com.vaadin.terminal.SystemError;
-import com.vaadin.terminal.UserError;
+import com.vaadin.data.util.ObjectProperty;
+import com.vaadin.ui.Accordion;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.Form;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.ListSelect;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.Window;
 
 import de.escidoc.admintool.app.AdminToolApplication;
 import de.escidoc.admintool.service.ContextService;
 import de.escidoc.admintool.service.OrgUnitService;
 import de.escidoc.admintool.view.ViewConstants;
 import de.escidoc.admintool.view.orgunit.OrgUnitAddView;
-import de.escidoc.admintool.view.validator.EmptyStringValidator;
-import de.escidoc.core.client.exceptions.EscidocException;
-import de.escidoc.core.client.exceptions.InternalClientException;
-import de.escidoc.core.client.exceptions.TransportException;
+import de.escidoc.admintool.view.util.LayoutHelper;
 import de.escidoc.core.resources.om.context.AdminDescriptors;
-import de.escidoc.core.resources.om.context.Context;
 import de.escidoc.core.resources.om.context.OrganizationalUnitRefs;
 
 @SuppressWarnings("serial")
-public class ContextAddView extends Form implements ClickListener {
-
+public class ContextAddView extends CustomComponent implements ClickListener {
+	 private final Logger log =
+	        LoggerFactory.getLogger(ContextAddView.class);
     private final AdminToolApplication app;
-
     private final ContextListView contextListView;
-
     private final ContextService contextService;
-
     private AdminDescriptorsAddView adminDescriptorsAddView;
-
     private OrgUnitAddView orgUnitAddView;
-
     private final OrgUnitService orgUnitService;
-
+    private TextField nameField;
+    private TextField descriptionField;
+    private TextField typeField;
+    
+    private ObjectProperty nameProperty;
+    private ObjectProperty descriptionProperty;
+	private ObjectProperty typeProperty;
+	private Object[][] orgUnits;
+    
     public ContextAddView(final AdminToolApplication app,
         final ContextListView contextListView,
         final ContextService contextService, final OrgUnitService orgUnitService) {
@@ -48,64 +58,164 @@ public class ContextAddView extends Form implements ClickListener {
         this.contextListView = contextListView;
         this.contextService = contextService;
         this.orgUnitService = orgUnitService;
-        buildUI();
+        init();
     }
 
     private void buildUI() {
-        setCaption("Add a new Context");
-        addNameField()
-            .addDescriptionField().addType().addOrgUnits().addAdminDescriptos()
-            .addFooter();
+    	addAdminDescriptos().addFooter();
 
-        setWriteThrough(false);
-        setInvalidCommitted(false);
+//        setWriteThrough(false);
+//  	setInvalidCommitted(false);
         // setValidationVisible(true);
     }
 
-    private ContextAddView addType() {
-        final TextField typeField = new TextField(ViewConstants.TYPE_LABEL);
-        addField(ViewConstants.TYPE_ID, typeField);
-        typeField.setWidth("400px");
-        typeField.setRequired(true);
-        typeField.addValidator(new EmptyStringValidator(ViewConstants.TYPE_LABEL
-            + " can not be empty."));
-        typeField.setRequiredError("Please enter a " + ViewConstants.TYPE_ID);
-        return this;
+    private ObjectProperty mapBinding(String initText, TextField tf){
+    	ObjectProperty op = new ObjectProperty(initText, String.class);
+        tf.setPropertyDataSource(op);
+    	return op; 
     }
 
-    private ContextAddView addOrgUnits() {
-        orgUnitAddView = new OrgUnitAddView(this, orgUnitService);
-        return this;
+    private void init(){
+    	Panel panel = new Panel();
+    	FormLayout form = new FormLayout();
+    	panel.setContent(form);
+    	form.setSpacing(false);
+    	panel.setCaption("Add a new Context");
+    	nameField = new TextField();
+        nameField.setWidth("400px");
+    	panel.addComponent(LayoutHelper.create(ViewConstants.NAME_LABEL, nameField, "150px", true));
+        nameProperty = mapBinding("", nameField);
+
+        descriptionField = new TextField();
+        descriptionField.setWidth("400px");
+        descriptionField.setRows(3);
+    	panel.addComponent(LayoutHelper.create(ViewConstants.DESCRIPTION_LABEL, descriptionField, "150px", true));
+    	descriptionProperty = mapBinding("", descriptionField);
+        
+        typeField = new TextField();
+        typeField.setWidth("400px");
+    	panel.addComponent(LayoutHelper.create(ViewConstants.TYPE_LABEL, typeField, "150px", true));
+    	typeProperty = mapBinding("", typeField);
+
+        final ListSelect select = new ListSelect();
+    	panel.addComponent(LayoutHelper.create(ViewConstants.ORGANIZATION_UNITS_LABEL, select, "150px", true));
+        select.setRows(5);
+        select.setWidth("300px");
+        select.setNullSelectionAllowed(true);
+        select.setMultiSelect(true);
+        select.setImmediate(true);
+        select.getValue();
+        
+        Button addOrgUnitButton = new Button("Add");
+        
+
+        
+        
+        addOrgUnitButton.addListener(new Button.ClickListener() {
+			public void buttonClick(ClickEvent event) {
+				final Window openTreeButtonWindow = new Window(ViewConstants.ORGANIZATION_UNITS_LABEL); //$NON-NLS-1$
+				openTreeButtonWindow.setModal(true);
+		    	final OrgUnitTree tree = new OrgUnitTree(openTreeButtonWindow);
+				/* Set window size. */
+				openTreeButtonWindow.setHeight("650px"); //$NON-NLS-1$
+				openTreeButtonWindow.setWidth("550px"); //$NON-NLS-1$
+				GridLayout grid = new GridLayout(1,2);
+		        grid.addComponent(tree, 0, 0);
+		        grid.setComponentAlignment(tree, Alignment.MIDDLE_CENTER);
+		        grid.setSizeUndefined();
+		        grid.setSizeFull();
+				openTreeButtonWindow.setContent(grid);
+		    	Button okButton = new Button("OK");
+		    	okButton.addListener(new Button.ClickListener() {
+					
+					public void buttonClick(ClickEvent event) {
+						Object o = tree.getSelectedItems();
+						o.getClass().toString();
+						if (o instanceof HashSet){
+							HashSet<String> set = (HashSet<String>)o;
+							for (String str : set){
+								select.addItem(str);
+							}
+						} else if (o instanceof Set){
+							Set<String> set = (Set<String>)o;
+							for (String str : set){
+								select.addItem(str);
+							}
+						} else if (o instanceof Object){
+							select.addItem(o);
+						}
+						((Window) openTreeButtonWindow.getParent()).removeWindow(openTreeButtonWindow);
+					}
+				});
+		    	Button cancelButton = new Button("Cancel");
+		    	cancelButton.addListener(new Button.ClickListener() {
+					
+					public void buttonClick(ClickEvent event) {
+						((Window) openTreeButtonWindow.getParent()).removeWindow(openTreeButtonWindow);
+					}
+				});
+		    	
+		    	HorizontalLayout hor = LayoutHelper.create("", "", okButton, cancelButton, "10px", false);
+				grid.addComponent(hor, 0, 1);
+		    	grid.setComponentAlignment(hor, Alignment.BOTTOM_LEFT);
+				getApplication().getMainWindow().addWindow(openTreeButtonWindow);
+			}
+		});
+        Button removeOrgUnitButton = new Button("Remove");
+        panel.addComponent(LayoutHelper.create("", "", addOrgUnitButton, removeOrgUnitButton, "150px", false));
+
+    	
+		Accordion accordion = new Accordion();
+		// Have it take all space available in the layout.
+		accordion.setSizeFull();
+
+//		accordion.setHeight("300px");
+		accordion.setWidth("800px");
+
+		// Trim its layout to allow the Accordion take all space.
+//		VerticalLayout adminDesc = new VerticalLayout(); //$NON-NLS-1$
+//		adminDesc.setSizeUndefined();
+//		adminDesc.setWidth("300px"); //$NON-NLS-1$
+//		adminDesc.setHeight("400px"); //$NON-NLS-1$
+//		adminDesc.addComponent(accordion);		
+	
+//		adminDesc.setSizeFull();
+//		adminDesc.getLayout().setMargin(false);
+		
+		panel.addComponent(LayoutHelper.create("Admin Descriptors", accordion, "150px", 300, true));		
+
+//		Panel pan = new Panel();
+//		pan.addComponent(accordion);
+//		pan.setScrollable(true);
+//		pan.setSizeUndefined();
+//		pan.setSizeFull();
+//		panel.addComponent(pan);
+
+        
+        panel.addComponent(addFooter());
+    	setCompositionRoot(panel);
     }
+    
+    
+//nameField.addValidator(new EmptyStringValidator("Name can not be empty."));
+//            nameField.setRequiredError("Please enter a " + ViewConstants.NAME_ID);
+//  descriptionField.addValidator(new EmptyStringValidator("Description can not be empty."));
+//  descriptionField.setRequiredError("Please enter a "+ ViewConstants.DESCRIPTION_ID);
+//    typeField.addValidator(new EmptyStringValidator(ViewConstants.TYPE_LABEL
+//            + " can not be empty."));
+//        typeField.setRequiredError("Please enter a " + ViewConstants.TYPE_ID);
+    
+
+//    private ContextAddView addOrgUnits() {
+//        orgUnitAddView = new OrgUnitAddView(this, orgUnitService);
+//        return this;
+//    }
 
     private ContextAddView addAdminDescriptos() {
         adminDescriptorsAddView = new AdminDescriptorsAddView(this);
         return this;
     }
 
-    private ContextAddView addNameField() {
-        final TextField nameField = new TextField(ViewConstants.NAME_LABEL);
-        addField(ViewConstants.NAME_ID, nameField);
-        nameField.setWidth("400px");
-        nameField.setRequired(true);
-        nameField.addValidator(new EmptyStringValidator("Name can not be empty."));
-        nameField.setRequiredError("Please enter a " + ViewConstants.NAME_ID);
-        return this;
-    }
-
-    private ContextAddView addDescriptionField() {
-        final TextField descriptionField =
-            new TextField(ViewConstants.DESCRIPTION_LABEL);
-        addField(ViewConstants.DESCRIPTION_ID, descriptionField);
-        descriptionField.setRequired(true);
-        descriptionField.setWidth("400px");
-        descriptionField.setRows(3);
-        descriptionField.addValidator(new EmptyStringValidator(
-            "Description can not be empty."));
-        descriptionField.setRequiredError("Please enter a "
-            + ViewConstants.DESCRIPTION_ID);
-        return this;
-    }
 
     private HorizontalLayout footer;
 
@@ -113,14 +223,15 @@ public class ContextAddView extends Form implements ClickListener {
 
     private final Button cancel = new Button("Cancel", (ClickListener) this);
 
-    private void addFooter() {
+    private HorizontalLayout addFooter() {
         footer = new HorizontalLayout();
         footer.setSpacing(true);
 
         footer.addComponent(save);
         footer.addComponent(cancel);
 
-        setFooter(footer);
+//        setFooter(footer);
+        return footer;
     }
 
     public void buttonClick(final ClickEvent event) {
@@ -137,7 +248,8 @@ public class ContextAddView extends Form implements ClickListener {
     }
 
     private void save() {
-        if (isValid()) {
+        /*
+    	if (isValid()) {
             try {
                 final String contextName = enteredName();
                 final String contextDescription = enteredDescription();
@@ -178,10 +290,12 @@ public class ContextAddView extends Form implements ClickListener {
         else {
             setValidationVisible(true);
         }
+        */
     }
 
     private String enteredType() {
-        return (String) getField(ViewConstants.TYPE_ID).getValue();
+//        return (String) getField(ViewConstants.TYPE_ID).getValue();
+    	return "";
     }
 
     private AdminDescriptors enteredAdminDescriptors()
@@ -194,17 +308,19 @@ public class ContextAddView extends Form implements ClickListener {
     }
 
     private String enteredName() {
-        return (String) getField(ViewConstants.NAME_ID).getValue();
+    	return "";
+//       return (String) getField(ViewConstants.NAME_ID).getValue();
     }
 
     private String enteredDescription() {
-        return (String) getField(ViewConstants.DESCRIPTION_ID).getValue();
+    	return "";
+//      return (String) getField(ViewConstants.DESCRIPTION_ID).getValue();
     }
 
     public void clear() {
-        getField(ViewConstants.NAME_ID).setValue("");
-        getField(ViewConstants.DESCRIPTION_ID).setValue("");
-        getField(ViewConstants.TYPE_ID).setValue("");
+//        getField(ViewConstants.NAME_ID).setValue("");
+//        getField(ViewConstants.DESCRIPTION_ID).setValue("");
+//        getField(ViewConstants.TYPE_ID).setValue("");
         adminDescriptorsAddView.clear();
         setComponentError(null);
     }
