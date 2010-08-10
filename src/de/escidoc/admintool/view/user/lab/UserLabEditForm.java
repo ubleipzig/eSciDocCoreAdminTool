ac6983feb5cf2bf0c17fb13a45797bcf900829de
asp <asp>
2010-08-10 14:07:09 +0000

    formatted code.

--- src/de/escidoc/admintool/view/user/lab/UserLabEditForm.java
+++ src/de/escidoc/admintool/view/user/lab/UserLabEditForm.java
@@ -18,34 +18,48 @@ import com.vaadin.ui.TextField;
 import de.escidoc.admintool.app.AdminToolApplication;
 import de.escidoc.admintool.service.UserService;
 import de.escidoc.admintool.view.ViewConstants;
-import de.escidoc.admintool.view.util.Converter;
-import de.escidoc.admintool.view.util.LayoutHelper;
 import de.escidoc.admintool.view.validator.EmptyFieldValidator;
 import de.escidoc.core.client.exceptions.EscidocClientException;
 import de.escidoc.core.client.exceptions.EscidocException;
 import de.escidoc.core.client.exceptions.InternalClientException;
 import de.escidoc.core.client.exceptions.TransportException;
 import de.escidoc.core.resources.aa.useraccount.UserAccount;
+import de.escidoc.vaadin.utilities.Converter;
+import de.escidoc.vaadin.utilities.LayoutHelper;
 
 @SuppressWarnings("serial")
 public class UserLabEditForm extends CustomComponent implements ClickListener {
-	private static final Logger log =
-        LoggerFactory.getLogger(UserLabEditForm.class);
+    private static final Logger log = LoggerFactory
+        .getLogger(UserLabEditForm.class);
+
     private final AdminToolApplication app;
+
     private final UserService userService;
+
     private HorizontalLayout footer;
-    private final Button save = new Button("Save", (ClickListener) this);
-    private final Button cancel = new Button("Cancel", (ClickListener) this);
-    private TextField nameField; 
+
+    private final Button save = new Button("Save", this);
+
+    private final Button cancel = new Button("Cancel", this);
+
+    private TextField nameField;
+
     private TextField loginNameField;
+
     private TextField objIdField;
+
     private Label modifiedOn;
+
     private Label modifiedBy;
+
     private Label createdOn;
+
     private Label createdBy;
+
     private CheckBox state;
+
     private Item item;
- 
+
     public UserLabEditForm(final AdminToolApplication app,
         final UserService userService) {
         this.app = app;
@@ -54,44 +68,49 @@ public class UserLabEditForm extends CustomComponent implements ClickListener {
         init();
     }
 
-    public void init(){
-    	Panel panel = new Panel();
-    	FormLayout form = new FormLayout();
-    	panel.setContent(form);
-    	form.setSpacing(false);
-    	
-    	panel.setCaption("Edit User Account");
-    	nameField = new TextField();
-    	nameField.setWidth("400px");
-    	nameField.setWriteThrough(false);
-    	
-        panel.addComponent(LayoutHelper.create(ViewConstants.NAME_LABEL, nameField, "100px", true));
+    public void init() {
+        Panel panel = new Panel();
+        FormLayout form = new FormLayout();
+        panel.setContent(form);
+        form.setSpacing(false);
+
+        panel.setCaption("Edit User Account");
+        nameField = new TextField();
+        nameField.setWidth("400px");
+        nameField.setWriteThrough(false);
+
+        panel.addComponent(LayoutHelper.create(ViewConstants.NAME_LABEL,
+            nameField, "100px", true));
         loginNameField = new TextField();
         loginNameField.setWidth("400px");
         loginNameField.setWriteThrough(false);
         loginNameField.setReadOnly(true);
-        panel.addComponent(LayoutHelper.create(ViewConstants.LOGIN_NAME_LABEL, loginNameField, "100px", false));
-        
+        panel.addComponent(LayoutHelper.create(ViewConstants.LOGIN_NAME_LABEL,
+            loginNameField, "100px", false));
+
         objIdField = new TextField();
         objIdField.setReadOnly(true);
-        panel.addComponent(LayoutHelper.create(ViewConstants.OBJECT_ID_LABEL, objIdField, "100px", false));
-        
+        panel.addComponent(LayoutHelper.create(ViewConstants.OBJECT_ID_LABEL,
+            objIdField, "100px", false));
+
         modifiedOn = new Label();
         modifiedBy = new Label();
-        panel.addComponent(LayoutHelper.create("Modified", "by", modifiedOn, modifiedBy, "100px", "15px", false));
-        
+        panel.addComponent(LayoutHelper.create("Modified", "by", modifiedOn,
+            modifiedBy, "100px", "15px", false));
+
         createdOn = new Label();
         createdBy = new Label();
-        panel.addComponent(LayoutHelper.create("Created", "by", createdOn, createdBy, "100px", "15px", false));
-        
+        panel.addComponent(LayoutHelper.create("Created", "by", createdOn,
+            createdBy, "100px", "15px", false));
+
         state = new CheckBox();
         state.setWriteThrough(false);
-        panel.addComponent(LayoutHelper.create("Active status", state, "100px", false));
-        
-        
+        panel.addComponent(LayoutHelper.create("Active status", state, "100px",
+            false));
+
         panel.addComponent(addFooter());
         setCompositionRoot(panel);
-        
+
     }
 
     private HorizontalLayout addFooter() {
@@ -106,59 +125,79 @@ public class UserLabEditForm extends CustomComponent implements ClickListener {
     public void buttonClick(final ClickEvent event) {
         final Button source = event.getButton();
         if (source == cancel) {
-        	nameField.setValue("");
+            nameField.setValue("");
             loginNameField.setValue("");
         }
         else if (source == save) {
             try {
-            	boolean valid = true;
-               	valid = EmptyFieldValidator.isValid(nameField, "Please enter a " + ViewConstants.NAME_ID);
-               	valid &= (EmptyFieldValidator.isValid(loginNameField, "Please enter a " + ViewConstants.LOGIN_NAME_ID));
-               	if (valid){
-                	 userService.update(getSelectedItemId(), (String)item.getItemProperty(ViewConstants.NAME_ID).getValue());
-                	 if(state.isModified()){
-                		 changeState();
-                	 }
+                boolean valid = true;
+                valid =
+                    EmptyFieldValidator.isValid(nameField, "Please enter a "
+                        + ViewConstants.NAME_ID);
+                valid &=
+                    (EmptyFieldValidator.isValid(loginNameField,
+                        "Please enter a " + ViewConstants.LOGIN_NAME_ID));
+                if (valid) {
+                    userService.update(getSelectedItemId(), (String) item
+                        .getItemProperty(ViewConstants.NAME_ID).getValue());
+                    if (state.isModified()) {
+                        changeState();
+                    }
                 }
                 nameField.setComponentError(null);
                 loginNameField.setComponentError(null);
-            	nameField.commit();
-            	loginNameField.commit();
+                nameField.commit();
+                loginNameField.commit();
             }
             catch (final EscidocException e) {
-            	log.error("An unexpected error occured! See log for details.", e);
+                log.error("An unexpected error occured! See log for details.",
+                    e);
                 e.printStackTrace();
             }
             catch (final InternalClientException e) {
-            	log.error("An unexpected error occured! See log for details.", e);
+                log.error("An unexpected error occured! See log for details.",
+                    e);
                 e.printStackTrace();
             }
             catch (final TransportException e) {
-            	log.error("An unexpected error occured! See log for details.", e);
+                log.error("An unexpected error occured! See log for details.",
+                    e);
                 e.printStackTrace();
-            } catch (EscidocClientException e) {
-            	log.error("An unexpected error occured! See log for details.", e);
-				e.printStackTrace();
-			}
+            }
+            catch (EscidocClientException e) {
+                log.error("An unexpected error occured! See log for details.",
+                    e);
+                e.printStackTrace();
+            }
         }
     }
 
     public void setSelected(final Item item) {
-    	this.item = item;
-    	if (item != null){
-    		nameField.setPropertyDataSource(item.getItemProperty(ViewConstants.NAME_ID));
-    		loginNameField.setPropertyDataSource(item.getItemProperty("properties.loginName"));
-    		objIdField.setPropertyDataSource(item.getItemProperty("objid"));
-    		modifiedOn.setCaption(Converter.dateTimeToString((org.joda.time.DateTime)item.getItemProperty("lastModificationDate").getValue()));
-    	    modifiedBy.setPropertyDataSource(item.getItemProperty("properties.modifiedBy.objid"));
-            state.setPropertyDataSource(item.getItemProperty("properties.active"));
-            createdOn.setCaption(Converter.dateTimeToString((org.joda.time.DateTime)item.getItemProperty("properties.creationDate").getValue()));
-            createdBy.setPropertyDataSource(item.getItemProperty("properties.createdBy.objid"));
-    	}
+        this.item = item;
+        if (item != null) {
+            nameField.setPropertyDataSource(item
+                .getItemProperty(ViewConstants.NAME_ID));
+            loginNameField.setPropertyDataSource(item
+                .getItemProperty("properties.loginName"));
+            objIdField.setPropertyDataSource(item.getItemProperty("objid"));
+            modifiedOn.setCaption(Converter
+                .dateTimeToString((org.joda.time.DateTime) item
+                    .getItemProperty("lastModificationDate").getValue()));
+            modifiedBy.setPropertyDataSource(item
+                .getItemProperty("properties.modifiedBy.objid"));
+            state.setPropertyDataSource(item
+                .getItemProperty("properties.active"));
+            createdOn.setCaption(Converter
+                .dateTimeToString((org.joda.time.DateTime) item
+                    .getItemProperty("properties.creationDate").getValue()));
+            createdBy.setPropertyDataSource(item
+                .getItemProperty("properties.createdBy.objid"));
+        }
     }
-    
+
     private String getSelectedItemId() {
-    	if (item == null) return "";
+        if (item == null)
+            return "";
         return (String) item.getItemProperty("objid").getValue();
     }
 
@@ -167,12 +206,13 @@ public class UserLabEditForm extends CustomComponent implements ClickListener {
         return userService.delete(getSelectedItemId());
     }
 
-	public void changeState() throws InternalClientException, TransportException, EscidocClientException {
-		if (!(Boolean)state.getPropertyDataSource().getValue()){
-        	userService.activate(getSelectedItemId());
+    public void changeState() throws InternalClientException,
+        TransportException, EscidocClientException {
+        if (!(Boolean) state.getPropertyDataSource().getValue()) {
+            userService.activate(getSelectedItemId());
         }
         else {
-        	userService.deactivate(getSelectedItemId());
+            userService.deactivate(getSelectedItemId());
         }
-	}
+    }
 }
