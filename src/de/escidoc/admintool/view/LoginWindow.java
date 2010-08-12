1c8f30f6da3f86f3b4fbdbf5b79f25a6f77cb67f
asp <asp>
2010-08-12 11:25:46 +0000

    Added Logging. Added exception handling for IOException.

--- src/de/escidoc/admintool/view/LoginWindow.java
+++ src/de/escidoc/admintool/view/LoginWindow.java
@@ -1,12 +1,17 @@
 package de.escidoc.admintool.view;
 
+import java.io.IOException;
 import java.io.Serializable;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.vaadin.terminal.ExternalResource;
+import com.vaadin.terminal.Resource;
 import com.vaadin.ui.LoginForm;
+import com.vaadin.ui.LoginForm.LoginEvent;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
-import com.vaadin.ui.LoginForm.LoginEvent;
 
 import de.escidoc.admintool.app.AdminToolApplication;
 import de.escidoc.core.client.exceptions.InternalClientException;
@@ -15,10 +20,18 @@ import de.escidoc.core.client.exceptions.application.security.AuthenticationExce
 
 @SuppressWarnings("serial")
 public class LoginWindow extends Window implements Serializable {
+	private static final Logger log =
+        LoggerFactory.getLogger(LoginWindow.class);
+	private Resource mainResource;
+    
+	public Resource getMainResource() {
+		return mainResource;
+	}
 
-    public LoginWindow() {
+	public LoginWindow() {
         final VerticalLayout layout = new VerticalLayout();
         setContent(layout);
+        
 
         final LoginForm login = new LoginForm();
         login.setWidth("100%");
@@ -31,8 +44,11 @@ public class LoginWindow extends Window implements Serializable {
                         event.getLoginParameter("username"),
                         event.getLoginParameter("password"));
 
+//                    mainResource = new ExternalResource(
+//                          AdminToolApplication.getInstance().getURL());
                     LoginWindow.this.open(new ExternalResource(
                         AdminToolApplication.getInstance().getURL()));
+//                    getApplication().getMainWindow().removeWindow(LoginWindow.this);
                 }
                 catch (final AuthenticationException e) {
                     LoginWindow.this.showNotification(new Window.Notification(
@@ -52,7 +68,12 @@ public class LoginWindow extends Window implements Serializable {
                         "Internal Server Error", e.getMessage(),
                         Window.Notification.TYPE_ERROR_MESSAGE));
                     e.printStackTrace();
-                }
+                } catch (IOException e) {
+                    LoginWindow.this.showNotification(new Window.Notification(
+                            "Internal Server Error", e.getMessage(),
+                            Window.Notification.TYPE_ERROR_MESSAGE));
+                        e.printStackTrace();
+                    }
             }
         });
         layout.addComponent(login);
