c1c8247ba7a751d3ec88f628faa891b3ca7b6958
asp <asp>
2010-08-19 13:53:43 +0000

    replace with message.properties.

--- eSciDocAdminTool/src/main/java/de/escidoc/admintool/app/AdminToolApplication.java
+++ eSciDocAdminTool/src/main/java/de/escidoc/admintool/app/AdminToolApplication.java
@@ -1,6 +1,8 @@
 package de.escidoc.admintool.app;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,13 +17,14 @@ import com.vaadin.event.ItemClickEvent.ItemClickListener;
 import com.vaadin.service.ApplicationContext;
 import com.vaadin.terminal.ThemeResource;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.SplitPanel;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Button.ClickListener;
 
+import de.escidoc.admintool.messages.Messages;
 import de.escidoc.admintool.service.ContextService;
 import de.escidoc.admintool.service.OrgUnitService;
 import de.escidoc.admintool.service.RoleService;
@@ -31,11 +34,10 @@ import de.escidoc.admintool.view.NavigationTree;
 import de.escidoc.admintool.view.Toolbar;
 import de.escidoc.admintool.view.context.ContextAddView;
 import de.escidoc.admintool.view.context.ContextEditForm;
-import de.escidoc.admintool.view.context.ContextEditViewWithToolbar;
 import de.escidoc.admintool.view.context.ContextListView;
 import de.escidoc.admintool.view.context.ContextView;
 import de.escidoc.admintool.view.orgunit.OrgUnitAddView;
-import de.escidoc.admintool.view.orgunit.OrgUnitEditForm;
+import de.escidoc.admintool.view.orgunit.OrgUnitEditView;
 import de.escidoc.admintool.view.orgunit.OrgUnitList;
 import de.escidoc.admintool.view.orgunit.OrgUnitView;
 import de.escidoc.admintool.view.user.UserView;
@@ -50,25 +52,35 @@ import de.escidoc.core.client.exceptions.EscidocException;
 import de.escidoc.core.client.exceptions.InternalClientException;
 import de.escidoc.core.client.exceptions.TransportException;
 import de.escidoc.core.client.exceptions.application.security.AuthenticationException;
+import de.escidoc.core.resources.oum.OrganizationalUnit;
 import de.escidoc.core.test.client.EscidocClientTestBase;
 import de.escidoc.vaadin.dialog.ErrorDialog;
 
-@SuppressWarnings("serial")
 public class AdminToolApplication extends Application
     implements ApplicationContext.TransactionListener, ClickListener,
     ValueChangeListener, ItemClickListener {
-    private final Logger log =
-        LoggerFactory.getLogger(AdminToolApplication.class);
+    private static final long serialVersionUID = -8811871788712427578L;
+
+    private static final String LOGIN_URL =
+        "http://localhost:8080/aa/login?target=http://localhost:8181/AdminTool";
+
+    private static final String ESCIDOC_USER_HANDLE_URL =
+        "http://localhost:8181/AdminTool?eSciDocUserHandle=";
+
+    private final Logger log = LoggerFactory
+        .getLogger(AdminToolApplication.class);
 
     @Override
     public void init() {
-        log.info("Starting application...");
+        log.info("Starting application..."); //$NON-NLS-1$
         getContext().addTransactionListener(this);
-        logoutButton.setIcon(new ThemeResource("icons/32/user.png"));
+        String path = Messages.getString("AdminToolApplication.1");
+        logoutButton.setIcon(new ThemeResource(path)); //$NON-NLS-1$
         logoutButton.addListener(new Button.ClickListener() {
+            @Override
             public void buttonClick(final Button.ClickEvent event) {
                 AdminToolApplication.this.getMainWindow().showNotification(
-                    "Logout...");
+                    Messages.getString("AdminToolApplication.2")); //$NON-NLS-1$
                 AdminToolApplication.this.close();
             }
         });
@@ -124,6 +136,7 @@ public class AdminToolApplication extends Application
     private static ThreadLocal<AdminToolApplication> currentApplication =
         new ThreadLocal<AdminToolApplication>();
 
+    @Override
     public void transactionStart(
         final Application application, final Object transactionData) {
         if (application == AdminToolApplication.this) {
@@ -131,6 +144,7 @@ public class AdminToolApplication extends Application
         }
     }
 
+    @Override
     public void transactionEnd(
         final Application application, final Object transactionData) {
         if (application == AdminToolApplication.this) {
@@ -161,7 +175,7 @@ public class AdminToolApplication extends Application
             loadProtectedResources(authentication);
         }
         catch (final EscidocException e) {
-            log.error("An unexpected error occured! See log for details.", e);
+            log.error(Messages.getString("AdminToolApplication.3"), e); //$NON-NLS-1$
             e.printStackTrace();
         }
     }
@@ -214,21 +228,22 @@ public class AdminToolApplication extends Application
 
     private final NavigationTree tree = new NavigationTree(this);
 
-    private final SplitPanel horizontalSplit =
-        new SplitPanel(SplitPanel.ORIENTATION_HORIZONTAL);
+    private final SplitPanel horizontalSplit = new SplitPanel(
+        SplitPanel.ORIENTATION_HORIZONTAL);
 
     private void buildMainLayout() {
-        final Window window = new Window("Admin Tool Prototype");
+        final Window window =
+            new Window(Messages.getString("AdminToolApplication.4")); //$NON-NLS-1$
         window.setSizeFull();
         setMainWindow(window);
 
-        setTheme("contacts");
+        setTheme(Messages.getString("AdminToolApplication.5")); //$NON-NLS-1$
 
         final VerticalLayout layout = new VerticalLayout();
         layout.setSizeFull();
 
-        layout.addComponent(Toolbar
-            .createToolbar(getMainWindow(), logoutButton));
+        layout.addComponent(Toolbar.createToolbar(getMainWindow(),
+            new Button[] { logoutButton }));
         layout.addComponent(horizontalSplit);
         layout.setExpandRatio(horizontalSplit, 1);
 
@@ -238,9 +253,11 @@ public class AdminToolApplication extends Application
         getMainWindow().setContent(layout);
     }
 
-    private final Button logoutButton = new Button("Sign out");
+    private final Button logoutButton = new Button(
+        Messages.getString("AdminToolApplication.6")); //$NON-NLS-1$
 
-    private final Button newUserButton = new Button("Add User");
+    private final Button newUserButton = new Button(
+        Messages.getString("AdminToolApplication.7")); //$NON-NLS-1$
 
     /*
      * private HorizontalLayout createToolbar() { final HorizontalLayout layout
@@ -269,6 +286,7 @@ public class AdminToolApplication extends Application
         horizontalSplit.setSecondComponent(c);
     }
 
+    @Override
     public void itemClick(final ItemClickEvent event) {
         if (event.getSource() == tree) {
             final Object itemId = event.getItemId();
@@ -288,17 +306,13 @@ public class AdminToolApplication extends Application
                         e.printStackTrace();
                     }
                     catch (final InternalClientException e) {
-                        log
-                            .error(
-                                "An unexpected error occured! See log for details.",
-                                e);
+                        log.error(Messages.getString("AdminToolApplication.8"), //$NON-NLS-1$
+                            e);
                         e.printStackTrace();
                     }
                     catch (final TransportException e) {
-                        log
-                            .error(
-                                "An unexpected error occured! See log for details.",
-                                e);
+                        log.error(Messages.getString("AdminToolApplication.9"), //$NON-NLS-1$
+                            e);
                         e.printStackTrace();
                     }
                 }
@@ -306,7 +320,8 @@ public class AdminToolApplication extends Application
                     showUsersLabView();
                 }
                 else {
-                    throw new RuntimeException("Unknown ItemID.");
+                    throw new RuntimeException(
+                        Messages.getString("AdminToolApplication.10")); //$NON-NLS-1$
                 }
             }
         }
@@ -317,9 +332,12 @@ public class AdminToolApplication extends Application
     // TODO refactor to lazy init later.
     private OrgUnitList orgUnitList;
 
-    private OrgUnitEditForm orgUnitEditForm;
+    private OrgUnitEditView orgUnitEditForm;
 
     private OrgUnitAddView orgUnitAddForm;
+
+    private final List<OrganizationalUnit> todoList =
+        new ArrayList<OrganizationalUnit>();
 
     /*
      * View getters exist so we can lazily generate the views, resulting in
@@ -329,30 +347,32 @@ public class AdminToolApplication extends Application
         if (orgUnitView == null) {
             orgUnitList = new OrgUnitList(this, orgUnitService);
             try {
-                orgUnitEditForm = new OrgUnitEditForm(this, orgUnitService);
+                orgUnitEditForm = new OrgUnitEditView(this, orgUnitService);
             }
             catch (final EscidocException e) {
-                log.error("An unexpected error occured! See log for details.",
+                log.error(Messages.getString("AdminToolApplication.11"), //$NON-NLS-1$
                     e);
                 e.printStackTrace();
             }
             catch (final InternalClientException e) {
-                log.error("An unexpected error occured! See log for details.",
+                log.error(Messages.getString("AdminToolApplication.12"), //$NON-NLS-1$
                     e);
                 e.printStackTrace();
             }
             catch (final TransportException e) {
-                log.error("An unexpected error occured! See log for details.",
+                log.error(Messages.getString("AdminToolApplication.13"), //$NON-NLS-1$
                     e);
                 e.printStackTrace();
             }
             catch (final UnsupportedOperationException e) {
-                log.error("An unexpected error occured! See log for details.",
+                log.error(Messages.getString("AdminToolApplication.14"), //$NON-NLS-1$
                     e);
                 e.printStackTrace();
             }
             catch (final Exception e) {
-                new ErrorDialog(getMainWindow(), "Error", e.getMessage());
+                new ErrorDialog(
+                    getMainWindow(),
+                    Messages.getString("AdminToolApplication.15"), e.getMessage()); //$NON-NLS-1$
             }
             orgUnitView =
                 new OrgUnitView(this, orgUnitList, orgUnitEditForm,
@@ -370,21 +390,18 @@ public class AdminToolApplication extends Application
 
     private ContextEditForm contextForm;
 
-    private Component getContextView() throws EscidocException,
+    public ContextView getContextView() throws EscidocException,
         InternalClientException, TransportException {
         if (contextView == null) {
             contextList =
                 new ContextListView(this, contextService, orgUnitService);
             contextForm =
                 new ContextEditForm(this, contextService, orgUnitService);
-            final ContextEditViewWithToolbar contextEditView =
-                new ContextEditViewWithToolbar(contextForm);
             final ContextAddView contextAddView =
                 new ContextAddView(this, contextList, contextService,
                     orgUnitService);
             contextView =
-                new ContextView(this, contextList, contextEditView,
-                    contextAddView);
+                new ContextView(this, contextList, contextForm, contextAddView);
         }
         contextView.showContextAddView();
         return contextView;
@@ -399,7 +416,6 @@ public class AdminToolApplication extends Application
         if (orgUnitList == null) {
             orgUnitList = new OrgUnitList(this, orgUnitService);
         }
-
         return orgUnitList;
     }
 
@@ -423,20 +439,18 @@ public class AdminToolApplication extends Application
 
     // TODO fix this, why do we have to create the add form every time?
     private OrgUnitAddView getOrganizationalUnitAddForm() {
-        orgUnitAddForm =
-            new OrgUnitAddView(this, orgUnitService, orgUnitList
-                .getAllOrgUnits(), orgUnitList);
+        orgUnitAddForm = new OrgUnitAddView(this, orgUnitService);
         return orgUnitAddForm;
     }
 
+    @Override
     public void valueChange(final ValueChangeEvent event) {
         final Property property = event.getProperty();
         // TODO this is not OOP, redesign this part of code.
         if (property == orgUnitList) {
             final Item item = orgUnitList.getItem(orgUnitList.getValue());
 
-            if (item != orgUnitEditForm.getItemDataSource()) {
-                orgUnitEditForm.setItemDataSource(item);
+            if (item != null) {
                 orgUnitView.showEditView(item);
             }
         }
@@ -459,17 +473,20 @@ public class AdminToolApplication extends Application
             }
         }
         else {
-            throw new RuntimeException("Unknown property.");
+            throw new RuntimeException(
+                Messages.getString("AdminToolApplication.16")); //$NON-NLS-1$
         }
     }
 
+    @Override
     public void buttonClick(final ClickEvent event) {
         final Button source = event.getButton();
         if (source == newUserButton) {
             showUserAddForm();
         }
         else {
-            throw new RuntimeException("Unknown button" + source);
+            throw new RuntimeException(
+                Messages.getString("AdminToolApplication.17") + source); //$NON-NLS-1$
         }
     }
 
@@ -528,7 +545,6 @@ public class AdminToolApplication extends Application
 
     public Component newOrgUnitAddView() throws EscidocException,
         InternalClientException, TransportException {
-        return new OrgUnitAddView(this, orgUnitService, orgUnitService
-            .getOrganizationalUnits(), orgUnitList);
+        return new OrgUnitAddView(this, orgUnitService);
     }
 }
