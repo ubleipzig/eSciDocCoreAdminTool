f66f182be1dc5cfa2ec6560bf9aa6c356047fb7c
crh <christian.herlambang@fiz-karlsruhe.de>
2010-09-20 10:43:22 +0000

    initial

--- src/main/java/de/escidoc/admintool/app/Command.java
+++
@@ -1,7 +0,0 @@
-package de.escidoc.admintool.app;
-
-import de.escidoc.core.client.exceptions.EscidocClientException;
-
-public interface Command {
-    void execute() throws EscidocClientException;
-}
