83be543f14029d69a762f211f5f5d700d8e48b4b
crh <christian.herlambang@fiz-karlsruhe.de>
2011-02-14 11:32:26 +0000

    removed unnecassary code
    removed script

--- src/main/java/de/escidoc/admintool/view/admintask/ShowResultCommandImpl.java
+++ src/main/java/de/escidoc/admintool/view/admintask/ShowResultCommandImpl.java
@@ -2,7 +2,6 @@ package de.escidoc.admintool.view.admintask;
 
 import java.util.Collection;
 
-import com.google.common.base.Preconditions;
 import com.vaadin.ui.Label;
 
 import de.escidoc.core.resources.adm.LoadExamplesResult.Entry;
@@ -27,25 +26,8 @@ final class ShowResultCommandImpl
     public void execute(final Collection<?> entries) {
         for (final Object entry : entries) {
             if (entry instanceof Entry) {
-                // updateContainers((Entry) entry);
                 showLoadedExamplesResult((Entry) entry);
             }
-        }
-    }
-
-    private void updateContainers(final Entry entry) {
-        Preconditions.checkNotNull(entry, "entry is null: %s", entry);
-        if (entry.getResourceType() == null) {
-            return;
-        }
-        switch (entry.getResourceType()) {
-            case OrganizationalUnit:
-                addToContainer.execute(entry);
-                break;
-            case Context:
-                addToContextContainer.execute(entry);
-                break;
-
         }
     }
 
