bb43cf59c622c654099cc9f1864bd280cc4b6b25
crh <christian.herlambang@fiz-karlsruhe.de>
2011-05-30 12:30:37 +0000

    refactoring

--- eSciDocAdminTool/src/main/java/de/escidoc/admintool/service/UserService.java
+++ eSciDocAdminTool/src/main/java/de/escidoc/admintool/service/UserService.java
@@ -9,6 +9,8 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.apache.axis.types.NonNegativeInteger;
+import org.apache.axis.types.PositiveInteger;
 import org.joda.time.DateTime;
 
 import com.google.common.base.Preconditions;
@@ -37,8 +39,7 @@ public class UserService {
 
     private UserAccountHandlerClientInterface client;
 
-    private final Map<String, UserAccount> userAccountById =
-        new ConcurrentHashMap<String, UserAccount>();
+    private final Map<String, UserAccount> userAccountById = new ConcurrentHashMap<String, UserAccount>();
 
     private final String eSciDocUri;
 
@@ -50,8 +51,7 @@ public class UserService {
 
     private GrantProperties grantProps;
 
-    public UserService(final String eSciDocUri, final String handle)
-        throws InternalClientException {
+    public UserService(final String eSciDocUri, final String handle) throws InternalClientException {
         this.eSciDocUri = eSciDocUri;
         this.handle = handle;
         initClient();
@@ -63,36 +63,45 @@ public class UserService {
         client.setHandle(handle);
     }
 
+    // public Collection<UserAccount> findAll() throws EscidocClientException {
+    // final SearchRetrieveRequestType filter = new SearchRetrieveRequestType();
+    // filter.setMaximumRecords(new NonNegativeInteger("20"));
+    // filter.setStartRecord(new PositiveInteger("1"));
+    // userAccounts = client.retrieveUserAccountsAsList(filter);
+    // for (final UserAccount user : userAccounts) {
+    // userAccountById.put(user.getObjid(), user);
+    // }
+    // return userAccounts;
+    // }
     public Collection<UserAccount> findAll() throws EscidocClientException {
-        userAccounts =
-            client.retrieveUserAccountsAsList(new SearchRetrieveRequestType());
+        final SearchRetrieveRequestType filter = new SearchRetrieveRequestType();
+        filter.setMaximumRecords(new NonNegativeInteger("20"));
+        filter.setStartRecord(new PositiveInteger("1"));
+        userAccounts = client.retrieveUserAccountsAsList(filter);
         for (final UserAccount user : userAccounts) {
             userAccountById.put(user.getObjid(), user);
         }
         return userAccounts;
     }
 
-    public UserAccount retrieve(final String userObjectId)
-        throws EscidocException, InternalClientException, TransportException {
+    public UserAccount retrieve(final String userObjectId) throws EscidocException, InternalClientException,
+        TransportException {
         return client.retrieve(userObjectId);
     }
 
-    public void update(final String objid, final String newName)
-        throws EscidocClientException {
+    public void update(final String objid, final String newName) throws EscidocClientException {
         assert !(newName == null || newName.isEmpty()) : "name must not be null or empty";
 
         // TODO name the class with its responsibility
         final UserAccount updatedUserAccount =
-            new UserAccountFactory()
-                .update(getSelectedUser(objid)).name(newName).build();
+            new UserAccountFactory().update(getSelectedUser(objid)).name(newName).build();
 
         client.update(updatedUserAccount);
     }
 
     // TODO ask Matthias, if we need one click button to activate OR deactivate
     // user account.
-    public void update(
-        final String objectId, final String newName, final Boolean isActive)
+    public void update(final String objectId, final String newName, final Boolean isActive)
         throws EscidocClientException {
 
         this.update(objectId, newName);
@@ -107,8 +116,7 @@ public class UserService {
 
     // TODO refactor to use polymorphism. Duplicate code in method body:
     // activate and deactivate
-    public void activate(final String selectedItemId)
-        throws InternalClientException, TransportException,
+    public void activate(final String selectedItemId) throws InternalClientException, TransportException,
         EscidocClientException {
 
         assert !(selectedItemId == null || selectedItemId.isEmpty()) : "selectedItemId must not be null or empty";
@@ -116,31 +124,26 @@ public class UserService {
         final UserAccount userAccount = getSelectedUser(selectedItemId);
         assert !userAccount.getProperties().isActive() : "User account is already active.";
 
-        client.activate(userAccount.getObjid(),
-            lastModificationDate(userAccount));
+        client.activate(userAccount.getObjid(), lastModificationDate(userAccount));
     }
 
-    public void deactivate(final String selectedItemId)
-        throws InternalClientException, TransportException,
+    public void deactivate(final String selectedItemId) throws InternalClientException, TransportException,
         EscidocClientException {
         assert !(selectedItemId == null || selectedItemId.isEmpty()) : "selectedItemId must not be null or empty";
 
         final UserAccount userAccount = getSelectedUser(selectedItemId);
         assert userAccount.getProperties().isActive() : "User account is not active.";
 
-        client.deactivate(userAccount.getObjid(),
-            lastModificationDate(userAccount));
+        client.deactivate(userAccount.getObjid(), lastModificationDate(userAccount));
     }
 
     private TaskParam lastModificationDate(final UserAccount userAccount) {
         final TaskParam taskParam = new TaskParam();
 
-        final DateTime lastModificationDate =
-            userAccount.getLastModificationDate();
+        final DateTime lastModificationDate = userAccount.getLastModificationDate();
         assert lastModificationDate != null : "last modification date has not been set";
 
-        taskParam
-            .setLastModificationDate(userAccount.getLastModificationDate());
+        taskParam.setLastModificationDate(userAccount.getLastModificationDate());
         return taskParam;
     }
 
@@ -151,13 +154,12 @@ public class UserService {
         return selectedUser;
     }
 
-    public UserAccount create(final String name, final String loginName)
-        throws EscidocException, InternalClientException, TransportException {
+    public UserAccount create(final String name, final String loginName) throws EscidocException,
+        InternalClientException, TransportException {
         assert !(name == null || name.isEmpty()) : "name can not be null or empty";
         assert !(loginName == null || loginName.isEmpty()) : "Login name can not be null or empty";
 
-        final UserAccount backedUserAccount =
-            new UserAccountFactory().create(name, loginName).build();
+        final UserAccount backedUserAccount = new UserAccountFactory().create(name, loginName).build();
 
         final UserAccount createdUserAccount = client.create(backedUserAccount);
         assert createdUserAccount != null : "Got null reference from the server.";
@@ -170,14 +172,12 @@ public class UserService {
         return createdUserAccount;
     }
 
-    public UserAccount delete(final String objectId)
-        throws EscidocClientException {
+    public UserAccount delete(final String objectId) throws EscidocClientException {
         client.delete(objectId);
         return userAccountById.remove(objectId);
     }
 
-    public UserAccount getUserById(final String objectId)
-        throws EscidocClientException {
+    public UserAccount getUserById(final String objectId) throws EscidocClientException {
 
         if (userAccounts == null) {
             findAll();
@@ -186,9 +186,8 @@ public class UserService {
         return userAccountById.get(objectId);
     }
 
-    public Collection<Grant> retrieveCurrentGrants(final String objectId)
-        throws InternalClientException, TransportException,
-        EscidocClientException {
+    public Collection<Grant> retrieveCurrentGrants(final String objectId) throws InternalClientException,
+        TransportException, EscidocClientException {
         return getRestClient().retrieveCurrentGrants(objectId).getGrants();
     }
 
@@ -197,8 +196,7 @@ public class UserService {
         return client;
     }
 
-    public void assign(final String userId, final String roleId)
-        throws EscidocClientException {
+    public void assign(final String userId, final String roleId) throws EscidocClientException {
         final Grant grant = new Grant();
         final GrantProperties gProp = new GrantProperties();
         gProp.setRole(new RoleRef(roleId));
@@ -219,8 +217,7 @@ public class UserService {
             throw new IllegalArgumentException("Role can not be null.");
         }
         if (user == null) {
-            throw new IllegalArgumentException(
-                "You must sign a role to a user.");
+            throw new IllegalArgumentException("You must sign a role to a user.");
         }
         grantProps = new GrantProperties();
         grantProps.setRole(new RoleRef(selectedRole.getObjid()));
@@ -240,25 +237,21 @@ public class UserService {
         client.createGrant(user.getObjid(), grant);
     }
 
-    public void revokeGrant(
-        final String userId, final Grant grant, final String comment)
-        throws EscidocClientException {
+    public void revokeGrant(final String userId, final Grant grant, final String comment) throws EscidocClientException {
         final TaskParam tp = new TaskParam();
         tp.setLastModificationDate(grant.getLastModificationDate());
         tp.setComment(comment);
         client.revokeGrant(userId, grant.getObjid(), tp);
     }
 
-    public void updatePassword(final UserAccount user, final String newPassword)
-        throws EscidocClientException {
+    public void updatePassword(final UserAccount user, final String newPassword) throws EscidocClientException {
 
         preconditions(user, newPassword);
         client.updatePassword(user.getObjid(), with(user, newPassword));
     }
 
-    public void updatePassword(
-        final String userId, final String newPassword,
-        final DateTime lastModificationDate) throws EscidocClientException {
+    public void updatePassword(final String userId, final String newPassword, final DateTime lastModificationDate)
+        throws EscidocClientException {
         preconditions(userId, newPassword, lastModificationDate);
 
         final TaskParam taskParam = new TaskParam();
@@ -268,20 +261,14 @@ public class UserService {
         client.updatePassword(userId, taskParam);
     }
 
-    private void preconditions(
-        final String userId, final String newPassword,
-        final DateTime lastModificationDate) {
+    private void preconditions(final String userId, final String newPassword, final DateTime lastModificationDate) {
 
         Preconditions.checkNotNull(userId, "userId is null: %s", userId);
-        Preconditions.checkNotNull(newPassword, "newPassword is null: %s",
-            newPassword);
-        Preconditions.checkNotNull(lastModificationDate,
-            "lastModificationDate is null: %s", lastModificationDate);
+        Preconditions.checkNotNull(newPassword, "newPassword is null: %s", newPassword);
+        Preconditions.checkNotNull(lastModificationDate, "lastModificationDate is null: %s", lastModificationDate);
 
-        Preconditions.checkArgument(!userId.isEmpty(), "userId is empty: %s",
-            userId);
-        Preconditions.checkArgument(!newPassword.isEmpty(),
-            "newPassword is empty: %s", newPassword);
+        Preconditions.checkArgument(!userId.isEmpty(), "userId is empty: %s", userId);
+        Preconditions.checkArgument(!newPassword.isEmpty(), "newPassword is empty: %s", newPassword);
     }
 
     private TaskParam with(final UserAccount user, final String newPassword) {
@@ -294,20 +281,15 @@ public class UserService {
     private void preconditions(final UserAccount user, final String newPassword) {
 
         Preconditions.checkNotNull(user, "user is null: %s", user);
-        Preconditions.checkNotNull(newPassword, "newPassword is null: %s",
-            newPassword);
+        Preconditions.checkNotNull(newPassword, "newPassword is null: %s", newPassword);
 
-        Preconditions.checkNotNull(user.getObjid(),
-            "user.getObjid() is null: %s", user.getObjid());
-        Preconditions.checkArgument(!newPassword.isEmpty(),
-            "newPassword is empty: %s", newPassword);
+        Preconditions.checkNotNull(user.getObjid(), "user.getObjid() is null: %s", user.getObjid());
+        Preconditions.checkArgument(!newPassword.isEmpty(), "newPassword is empty: %s", newPassword);
     }
 
-    public List<String> retrieveOrgUnitsFor(final String objectId)
-        throws EscidocClientException {
+    public List<String> retrieveOrgUnitsFor(final String objectId) throws EscidocClientException {
         Preconditions.checkNotNull(objectId, "objectId is null: %s", objectId);
-        Preconditions.checkArgument(!objectId.isEmpty(), objectId,
-            "objectId is empty: %s");
+        Preconditions.checkArgument(!objectId.isEmpty(), objectId, "objectId is empty: %s");
 
         final List<String> orgUnits = new ArrayList<String>();
         for (final Attribute attribute : client.retrieveAttributes(objectId)) {
@@ -319,30 +301,24 @@ public class UserService {
         return orgUnits;
     }
 
-    public Attributes retrieveAttributes(final String objectId)
-        throws EscidocClientException {
+    public Attributes retrieveAttributes(final String objectId) throws EscidocClientException {
         return client.retrieveAttributes(objectId);
     }
 
     private boolean nameIsEqualsO(final Attribute attribute) {
         return !attribute.getName().isEmpty()
-            && AppConstants.ORGANIZATIONAL_UNIT_DEFAULT_ATTRIBUTE_NAME
-                .equals(attribute.getName());
+            && AppConstants.ORGANIZATIONAL_UNIT_DEFAULT_ATTRIBUTE_NAME.equals(attribute.getName());
     }
 
-    public void assign(final String objectId, final Attribute attribute)
-        throws EscidocClientException {
+    public void assign(final String objectId, final Attribute attribute) throws EscidocClientException {
         Preconditions.checkNotNull(objectId, "objectId is null: %s", objectId);
-        Preconditions.checkNotNull(attribute, "attribute is null: %s",
-            attribute);
+        Preconditions.checkNotNull(attribute, "attribute is null: %s", attribute);
         client.createAttribute(objectId, attribute);
     }
 
-    public void updateAttribute(final String objectId, final Attribute attribute)
-        throws EscidocClientException {
+    public void updateAttribute(final String objectId, final Attribute attribute) throws EscidocClientException {
         Preconditions.checkNotNull(objectId, "objectId is null: %s", objectId);
-        Preconditions.checkNotNull(attribute, "attribute is null: %s",
-            attribute);
+        Preconditions.checkNotNull(attribute, "attribute is null: %s", attribute);
         client.updateAttribute(objectId, attribute);
     }
 
@@ -350,11 +326,9 @@ public class UserService {
         return client.retrieveCurrentUser();
     }
 
-    public void removeAttribute(final String objectId, final Attribute attribute)
-        throws EscidocClientException {
+    public void removeAttribute(final String objectId, final Attribute attribute) throws EscidocClientException {
         Preconditions.checkNotNull(objectId, "objectId is null: %s", objectId);
-        Preconditions.checkNotNull(attribute, "attribute is null: %s",
-            attribute);
+        Preconditions.checkNotNull(attribute, "attribute is null: %s", attribute);
         client.deleteAttribute(objectId, attribute);
     }
 
